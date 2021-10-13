31fd8065bc6dfd446641f7634e5e06573d8efe47
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-10-13 15:32:34 -0400

    added project dependecies to our package.json

---
+++ build/precache-manifest.882b283c9f5c25ce0d48f776df51bfd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e85e5c3c36665abad58",
+    "url": "/IshtiaqueNafis/COMP3104/static/css/main.a6e259d1.chunk.css"
+  },
+  {
+    "revision": "5e85e5c3c36665abad58",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/main.5e85e5c3.chunk.js"
+  },
+  {
+    "revision": "cf02de6e9ac049f45562",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/runtime~main.cf02de6e.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "22f0a9171617d1453364552c67bb8072",
+    "url": "/IshtiaqueNafis/COMP3104/index.html"
+  }
+];
