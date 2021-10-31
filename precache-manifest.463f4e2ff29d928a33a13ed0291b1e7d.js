ad3498bdf7a4a37ee5dc8b8ef1186d29bb1302b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-31 11:24:49 +0000

    Deploy IshtiaqueNafis/COMP3104 to github.com/IshtiaqueNafis/COMP3104.git:gh-pages

---
+++ precache-manifest.463f4e2ff29d928a33a13ed0291b1e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ebc48cabec3979f0f2",
+    "url": "/IshtiaqueNafis/COMP3104/static/css/main.df98dcff.chunk.css"
+  },
+  {
+    "revision": "f1ebc48cabec3979f0f2",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/main.f1ebc48c.chunk.js"
+  },
+  {
+    "revision": "cf02de6e9ac049f45562",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/runtime~main.cf02de6e.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/IshtiaqueNafis/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "eaf1e5bd57a273ac5cd44bf9ee8d8ac5",
+    "url": "/IshtiaqueNafis/COMP3104/index.html"
+  }
+];
